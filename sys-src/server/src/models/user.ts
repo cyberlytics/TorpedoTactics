dd914bd30d6dba6f257bcb3c529ddfb366a60a88
Alexander Hammer <a.hammer@oth-aw.de>
2023-05-21 10:24:11 +0200

    Datenbankmodels hinzugefügt
    Former-commit-id: 8963a0bea0d84b58d5e966b244c01e6af7f17f56

--- sys-src/server/src/models/user.ts
+++ sys-src/server/src/models/user.ts
@@ -1,14 +1,15 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema} from "mongoose";
 
-const userSchema : Schema = new Schema({
+interface IUser{
+    username: string;
+    password: string;
+}
+
+const userSchema : Schema = new Schema<IUser>({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true, unique: true},
 });
 
-interface IUser extends Document{
-    username: string;
-    password: string;
-}
 
 export const User = mongoose.model<IUser>('User',userSchema);
 
