4de0a546e309c2b5893d5ee58eebb51f8a48e03d
Berkay Yurdagül <b.yurdaguel@oth-aw.de>
2023-05-22 15:00:21 +0200

    Update server.ts
    Former-commit-id: e713c6d8807f9263b7817bb9244a0a05d127ebed

--- sys-src/server/src/server.ts
+++ sys-src/server/src/server.ts
@@ -9,10 +9,7 @@ const start = async () => {
   })
   } catch (err){
      // close server
-     app.close(() => {
-      console.error(err)
-      process.exit(0)
-    })
+      console.error(err);
   } 
 
   app.listen(3000, () => {
