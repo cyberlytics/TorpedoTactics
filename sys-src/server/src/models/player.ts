59f50065daeade3f3b6226fa98971c573a41cb46
Alexander Hammer <a.hammer@oth-aw.de>
2023-05-24 13:33:56 +0200

    Beispielmethoden Game und Player Model
    Former-commit-id: 17384cc2d78aebf9f1c70fdd10745e88abc295b5

--- sys-src/server/src/models/player.ts
+++ sys-src/server/src/models/player.ts
@@ -1,20 +1,46 @@
-import mongoose, { Schema} from "mongoose";
+import mongoose, { Schema, Model, HydratedDocument} from "mongoose";
 
-interface IPlayer{
-    userid: Schema.Types.ObjectId;
+export interface IPlayer{
+    userid?: Schema.Types.ObjectId;
     statistics:{
         gamesplayed: number,
         gameswon: number,
         timespend: number,
         hitmissrate: number,
     },
-    games: Schema.Types.ObjectId[],
+    games?: Schema.Types.ObjectId[],
 }
 
-const playerSchema : Schema = new Schema<IPlayer>({
-    userid: {type: Schema.Types.ObjectId, required: true, unique: true, ref: 'User'},
+//methods for a Player Instance (Document)
+export interface IPlayerMethods {
+
+}
+
+//methods on all Players 
+interface IPlayerModel extends Model<IPlayer, {}, IPlayerMethods>{
+    getPlayers() : Promise<HydratedDocument<IPlayer, IPlayerMethods>[]>;
+    addPlayer(): Promise<HydratedDocument<IPlayer, IPlayerMethods>>
+}
+
+//userid should be unique, only for developing
+const playerSchema : Schema<IPlayer, IPlayerModel> = new Schema<IPlayer, IPlayerModel>({
+    userid: {type: Schema.Types.ObjectId, ref: 'User'},
     statistics : {type: Object, required: true},
     games: [{type: Schema.Types.ObjectId, ref: 'Game'}],
 });
 
-export const Player = mongoose.model<IPlayer>('Player', playerSchema);
+
+//static methods
+playerSchema.statics.addPlayer = async function(): Promise<HydratedDocument<IPlayer, IPlayerMethods>> {
+    const newPlayer : IPlayer = {statistics: {gamesplayed:0, gameswon: 0, timespend: 0.0, hitmissrate:0.0}};
+    return await this.create(newPlayer);
+}
+
+playerSchema.statics.getPlayers = async function():Promise<HydratedDocument<IPlayer, IPlayerMethods>[]> {
+    return await this.find();
+};
+
+//instance methods
+
+//export model
+export const Player = mongoose.model<IPlayer, IPlayerModel>('Player', playerSchema);
