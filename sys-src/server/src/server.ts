65b78c4ebb4fe4741832cccd719e2123116d2835
Lukas Feil <l.feil@oth-aw.de>
2023-05-31 20:08:25 +0200

    Former-commit-id: f335f5b80e905954fbf17a2d9033aa6ace8df4e7

--- sys-src/server/src/server.ts
+++ sys-src/server/src/server.ts
@@ -1,29 +1,18 @@
 import { app } from './app'
 import { SocketManager } from './services/socketManager';
-import mongoose, {HydratedDocument} from 'mongoose';
-import { IUser,User, IUserMethods } from './models/user';
-//import { IPlayer,Player } from './models/player'
+import mongoose from 'mongoose';
 
 const start = async () => {
   try{
     await mongoose.connect("mongodb+srv://gruppegruen:TorpedoTactics@bcn.xuho2ki.mongodb.net/?retryWrites=true&w=majority")
   .then(async() => {
     console.log('Verbindung zur MongoDB hergestellt');
-
-  //Example call static method
-   const users : HydratedDocument<IUser, IUserMethods>[] = await User.getUsers();
-   console.log(users);
-   //Example call instance method with promise processing
-   let user : HydratedDocument<IUser, IUserMethods> | null = await User.getUserByName('test');
-   //Only the user
-   let realUser = user!.toObject() as IUser;
-   console.log(realUser);
   })
 
     // initialize SocketManager
     new SocketManager().initialize();
   } catch (err){
-      console.error(err);
+    console.error(err);
   }
 
   app.listen(3000, () => {
