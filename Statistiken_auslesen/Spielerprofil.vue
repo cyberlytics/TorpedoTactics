ef238bc9d5228f181c7652b8e5f6adb68bee9b53
Timon Spichtinger <t.spichtinger@oth-aw.de>
2023-06-11 19:55:32 +0100

    Ich hab mit vue.js angefangen. noch nicht testfähig
    Former-commit-id: 54fd3661db936ee372dab34e42e3a0ebfdb0e94b

--- Statistiken_auslesen/Spielerprofil.vue
+++ Statistiken_auslesen/Spielerprofil.vue
@@ -1,79 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Spielerstatistiken</title>
-  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-  <style>
-    .gewonnen-balken {
-      background-color: green;
-      height: 10px;
-    }
-
-    .verloren-balken {
-      background-color: red;
-      height: 10px;
-    }
-  </style>
-</head>
-
-<body>
-<div id="app">
-  <h1>Spielerstatistiken</h1>
-  <div v-for="spieler in spielerprofile" :key="spieler.id">
-    <h2>Spieler:  {{ spieler.id }}</h2>
-    <p>Trefferrate: {{ spieler.trefferrate }}%</p>
-    <p>Gewonnene Spiele: {{ spieler.gewonneneSpiele }}</p>
-    <p>Verlorene Spiele: {{ spieler.verloreneSpiele }}</p>
-    <div style="display: flex;">
-      <div>{{ spieler.anteilGewonnen * 100 + '%' }}</div>
-      <div class="gewonnen-balken" :style="{ width: spieler.anteilGewonnen * 90 + '%' }"></div>
-      <div class="verloren-balken" :style="{ width: spieler.anteilVerloren * 90 + '%' }"></div>
-      <div>{{ spieler.anteilVerloren * 100 + '%' }}</div>
-
+<template>
+  <div>
+    <h1>Spielerstatistiken</h1>
+    <div v-for="spieler in spielerprofile" :key="spieler.id">
+      <h2>Spieler:  {{ spieler.id }}</h2>
+      <p>Trefferrate: {{ spieler.trefferrate }}%</p>
+      <p>Gewonnene Spiele: {{ spieler.gewonneneSpiele }}</p>
+      <p>Verlorene Spiele: {{ spieler.verloreneSpiele }}</p>
+      <div style="display: flex;">
+        <div>{{ spieler.anteilGewonnen * 100 + '%' }}</div>
+        <div class="gewonnen-balken" :style="{ width: spieler.anteilGewonnen * 90 + '%' }"></div>
+        <div class="verloren-balken" :style="{ width: spieler.anteilVerloren * 90 + '%' }"></div>
+        <div>{{ spieler.anteilVerloren * 100 + '%' }}</div>
+      </div>
     </div>
   </div>
+</template>
 
-
-
-  <script>
-    new Vue({
-      el: '#app',
-      data: {
-        spielerprofile: [
-          {
-            // Beispielprofile
-            id: 1,
-            trefferrate: 75,
-            gewonneneSpiele: 10,
-            verloreneSpiele: 2,
-            anteilGewonnen: '',
-            anteilVerloren: '',
-          },
-          {
-            id: 2,
-            trefferrate: 60,
-            gewonneneSpiele: 5,
-            verloreneSpiele: 5,
-            anteilGewonnen: '',
-            anteilVerloren: '',
-          }
-        ]
-      },
-      mounted() {
-        this.spielerprofile.forEach(spieler => {
-          spieler.anteilGewonnen = (spieler.gewonneneSpiele / (spieler.gewonneneSpiele + spieler.verloreneSpiele)).toFixed(3);
-          spieler.anteilVerloren = (spieler.verloreneSpiele / (spieler.gewonneneSpiele + spieler.verloreneSpiele)).toFixed(3);
-        });
-      }
+<script>
+export default {
+  data() {
+    return {
+      spielerprofile: [
+        {
+          id: 1,
+          trefferrate: 75,
+          gewonneneSpiele: 10,
+          verloreneSpiele: 2,
+          anteilGewonnen: '',
+          anteilVerloren: '',
+        },
+        {
+          id: 2,
+          trefferrate: 60,
+          gewonneneSpiele: 5,
+          verloreneSpiele: 5,
+          anteilGewonnen: '',
+          anteilVerloren: '',
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.spielerprofile.forEach(spieler => {
+      spieler.anteilGewonnen = (spieler.gewonneneSpiele / (spieler.gewonneneSpiele + spieler.verloreneSpiele)).toFixed(3);
+      spieler.anteilVerloren = (spieler.verloreneSpiele / (spieler.gewonneneSpiele + spieler.verloreneSpiele)).toFixed(3);
     });
-  </script>
+  }
+};
+</script>
 
-</div>
-</body>
+<style>
+.gewonnen-balken {
+  background-color: green;
+  height: 10px;
+}
 
-</html>
-
-
+.verloren-balken {
+  background-color: red;
+  height: 10px;
+}
+</style>
