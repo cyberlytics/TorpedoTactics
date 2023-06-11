f26e6efdfa5f640dca9d172bc8d44e28299cd984
Timon Spichtinger <t.spichtinger@oth-aw.de>
2023-06-11 19:23:43 +0100

    Ich hab mit vue.js angefangen. noch nicht testfähig
    Former-commit-id: 907b74e2a60cf7011dce7064becbb0907c050e67

---
+++ Statistiken_auslesen/Spielerprofil.vue
@@ -0,0 +1,31 @@
+new Vue({
+  el: '#app',
+  data: {
+    spielerprofile: [
+      {
+        //beispielprofile
+        id: 1,
+        trefferrate: 75,
+        gewonneneSpiele: 10,
+        verloreneSpiele: 2,
+        anteilGewonnen: '',
+        anteilVerloren: '',
+      },
+      {
+        id: 2,
+        trefferrate: 60,
+        gewonneneSpiele: 5,
+        verloreneSpiele: 5,
+        anteilGewonnen: '',
+        anteilVerloren: '',
+      }
+    ]
+  },
+  mounted() {
+    this.spielerprofile.forEach(spieler => {
+      spieler.anteilGewonnen = (spieler.gewonneneSpiele / (spieler.gewonneneSpiele + spieler.verloreneSpiele)).toFixed(3);
+      spieler.anteilVerloren = (spieler.verloreneSpiele / (spieler.gewonneneSpiele + spieler.verloreneSpiele)).toFixed(3);
+
+    });
+  }
+});
