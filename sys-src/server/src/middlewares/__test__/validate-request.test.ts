acc9ce24ab445eaaa85ab4b76fbec5f431d62b87
Berkay Yurdagül <b.yurdaguel@oth-aw.de>
2023-06-14 13:56:02 +0200

    tests added
    Former-commit-id: 2bc0a3bea8062276bb6f4812ea691dd1ea1ab49f

--- sys-src/server/src/middlewares/__test__/validate-request.test.ts
+++ sys-src/server/src/middlewares/__test__/validate-request.test.ts
@@ -4,24 +4,27 @@ import { app } from '../../app'
 jest.mock("../../models/user", ()=>({
   create: jest.fn(()=>{return {}})
 }))
+
 describe('validateRequest', () => {
   beforeAll(()=>      {
     process.env.JWT_KEY="secret"
   })
-  // Let's assume there's a POST route in your application that uses the validateRequest middleware
-  it('should throw a RequestValidationError if validation fails', async () => {
-    const response = await request(app).post('/api/auth/signin').send({}) // Let's assume the route expects some data, and we're sending an empty object
-
-    expect(response.status).toBe(400) // Assuming your RequestValidationError sets the status code to 400
-    expect(response.body.errors).toBeDefined() // Assuming your error format includes an 'errors' property
-  })
 
   it('should not throw a RequestValidationError if validation is successful', async () => {
     const response = await request(app).post('/api/auth/signup').send({
-    password: 'passworD1!', username: '2'
-  }) // Replace with valid data for your route
+      password: 'passworD1!', username: '2'
+    })
 
     expect(response.status).not.toBe(400)
     expect(response.body.errors).toBeUndefined()
   })
+
+  it('should throw a RequestValidationError if validation fails', async () => {
+    const response = await request(app).post('/api/auth/signin').send({})
+
+    expect(response.status).toBe(400)
+    expect(response.body.errors).toBeDefined()
+  })
+
+
 })
